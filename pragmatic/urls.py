0bb85d7c38f1f6bbf105ba45d8ba72df5c5a67f1
chan <3233978@naver.com>
2022-06-05 00:43:36 +0900

    Starting Profileapp with ModelForm

--- pragmatic/urls.py
+++ pragmatic/urls.py
@@ -21,4 +21,6 @@ import accountapp
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accountapp.urls')),
+    path('profiles/', include('profileapp.urls')),
+
 ]
