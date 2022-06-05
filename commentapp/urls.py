f286674c4a4ff1d4108690049d9badf1de79e7c3
chan <3233978@naver.com>
2022-06-05 14:36:26 +0900

    Commentapp Implementation with Mixin

---
+++ commentapp/urls.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import path
+from django.views.generic import TemplateView
+
+
+from commentapp.views import CommentCreateView
+
+app_name = 'commentapp'
+
+urlpatterns = [
+    path('create/', CommentCreateView.as_view(), name='create'),
+
+]
