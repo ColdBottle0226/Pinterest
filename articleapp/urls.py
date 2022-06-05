fed6aaf87d5586ffdd23c2f3182f16f6aaf31707
chan <3233978@naver.com>
2022-06-05 10:37:26 +0900

    Articleapp Implementation with MagicGrid

---
+++ articleapp/urls.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import path
+from django.views.generic import TemplateView
+
+from accountapp.views import hello_world, AccountCreateView, AccountDetailView, AccountUpdateView, AccountDeleteView
+
+app_name = 'articleapp'
+
+urlpatterns = [
+    path('list/', TemplateView.as_view(template_name='articleapp/list.html'), name='list'),
+]
